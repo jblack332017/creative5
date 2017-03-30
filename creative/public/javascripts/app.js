b24cdc011af9b3c7b2e7d4b6d13ebac4bb9ef2fa
Josh Black <joshblack@tigger.local>
2017-03-29 21:50:07 -0600

    here

--- creative/public/javascripts/app.js
+++ creative/public/javascripts/app.js
@@ -2,21 +2,10 @@ $(document).ready(function(){
 
       $.getJSON('../reviews', function(data) {
       console.log(data);
-      var everything = "<ul>";
-      for(var comment in data) {
-      	console.log(comment);
-        com = data[comment];
-        console.log(com);
-        for (var item in com)
-        {
-        	console.log(item);
-        }
-        jobj = JSON.stringify(myobj);
-        console.log(jobj);
-        everything += "<li> Name: " + com.Name + " -- Comment: " + com.Review + "</li>";
-      }
-      everything += "</ul>";
-      $("#comments").html(everything);
+      jobj = JSON.stringify(data);
+      console.log(jobj);
+
+
     })
 
 
