77dac36de2a3f278a6370c08974d9ae76ee59668
Josh Black <joshblack@tigger.local>
2017-03-29 22:26:10 -0600

    here

--- creative/public/javascripts/app.js
+++ creative/public/javascripts/app.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
 
-      $.getJSON('../reviews', function(data) {
-      console.log(data);
-      for (review in data)
-      {
-      	
-      }
-
-
-    })
+      var myobj = {name:"#name",review:"#comment"};
+      jobj = JSON.stringify(myobj);
+      $("#json").text(jobj);
+	var url = "../reviews";
+$.ajax({
+url:url,
+type: "POST",
+data: jobj,
+contentType: "application/json; charset=utf-8",
+success: function(data,textStatus) {
+}
+})
 
 
 	});
