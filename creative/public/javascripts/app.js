b8b5feabb354fdb03dbecb5994f2094e5d6d6a0b
Josh Black <joshblack@tigger.local>
2017-03-30 07:56:03 -0600

    here

--- creative/public/javascripts/app.js
+++ creative/public/javascripts/app.js
@@ -5,16 +5,53 @@ $(document).ready(function(){
       jobj = JSON.stringify(myobj);
       $("#json").text(jobj);
 	var url = "../reviews";
-$.ajax({
-url:url,
-type: "POST",
-data: jobj,
-contentType: "application/json; charset=utf-8",
-success: function(data,textStatus) {
-    $("#done").html(textStatus);
-}
-})
-  });
+	$.ajax({
+	url:url,
+	type: "POST",
+	data: jobj,
+	contentType: "application/json; charset=utf-8",
+	success: function(data,textStatus) {
+	    $("#done").html(textStatus);
+	}
+	})
+	  });
+
+     $("#cancelReview").click(function(){
+     	$("#reviewContainer").css("display", "none");
+
+     });
 
 
-	});
+
+     $("#hotRod").click(function(){
+     	$("#reviewContainer").css("display", "block");
+     	$("#movie").text("Hot Rod");
+
+     });
+     $("#beauty").click(function(){
+     	$("#reviewContainer").css("display", "block");
+     	$("#movie").text("Beauty and the Beast");
+
+     });
+     $("#hackers").click(function(){
+     	$("#reviewContainer").css("display", "block");
+     	$("#movie").text("Hackers");
+
+     });
+     $("#finding").click(function(){
+     	$("#reviewContainer").css("display", "block");
+     	$("#movie").text("Finding Nemo");
+
+     });
+     $("#nacho").click(function(){
+     	$("#reviewContainer").css("display", "block");
+     	$("#movie").text("Nacho Libre");
+
+     });
+     $("#sand").click(function(){
+     	$("#reviewContainer").css("display", "block");
+     	$("#movie").text("The Sandlot");
+
+     });
+
+});
