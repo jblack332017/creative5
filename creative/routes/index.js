4e7d77af26467266e3a68174143eefb3215cb466
Josh Black <joshblack@tigger.local>
2017-03-29 21:11:49 -0600

    here

--- creative/routes/index.js
+++ creative/routes/index.js
@@ -41,17 +41,19 @@ router.post('/reviews', function(req, res) {
 });
 
 
-router.get('/reviews', function(req, res, next) {
-console.log("In the GET route?");
-collection.find(function(err,commentList) { //Calls the find() method on your database
-  if (err) return console.error(err); //If there's an error, print it out
-  else {
-    console.log(commentList); //Otherwise console log the comments you found
-	res.json(commentList); //Then send the comments
-    
-    
-  }
-})
+router.get('/reviews', function(req, res) {
+  console.log("In Pokemon");
+  collection.find(function(err, result) {
+    if(err) {
+      console.log(err);
+    } else if (result.length) {
+      console.log("Query Worked");
+      console.log(result);
+      res.send(result);
+    } else {
+      console.log("No Documents found");
+    }
+  });
 });
 
 
