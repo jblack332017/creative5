f59138aeff5144bd95cd4028622863a7fa47a862
Josh Black <joshblack@tigger.local>
2017-03-29 19:49:31 -0600

    here

--- creative/public/javascripts/app.js
+++ creative/public/javascripts/app.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
 
+	$.getJSON('../reviews', function(data) {
+      console.log(data);
+      var everything = "<ul>";
+      for(var comment in data) {
+        com = data[comment];
+        everything += "<li> Name: " + com.Name + " -- Comment: " + com.Review + "</li>";
+      }
+      everything += "</ul>";
+      $("#comments").html(everything);
+    })
+
 
 	});
