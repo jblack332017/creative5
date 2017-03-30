d7424f58b6d0ad204db7d6b5f3e216e6cbbff529
Josh Black <joshblack@Julias-MBP-010024163210.app.byu.edu>
2017-03-30 09:15:23 -0600

    here

--- creative/routes/index.js
+++ creative/routes/index.js
@@ -57,7 +57,7 @@ Review.find(function(err,commentList) { //Calls the find() method on your databa
 
 router.get('/reviews/:movie', function(req, res, next) {
 console.log("In the GET route?");
-console.log(req.movie);
+console.log(req.params.movie);
 Review.find({ 'movie': req.movie }, function(err,commentList) { //Calls the find() method on your database
   if (err) return console.error(err); //If there's an error, print it out
   else {
